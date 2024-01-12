1d034c2e6a87eb3213d7a058a379a9b392269ebc
min2023237 <min2023237@gmail.com>
2024-01-12 10:00:24 +0900

    리뷰용 코드 업로드

--- jpa-skeleton-main/src/main/java/com/example/jpa/repo/StudentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.jpa.repo;
-
-import com.example.jpa.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {}
